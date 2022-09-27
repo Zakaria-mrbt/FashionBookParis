f744227273db7cbe993f7af327926cb0e62335dc
13010-30-05 <13010-30-05@exchange.ad.afpanet>
2022-09-27 16:25:42 +0200

    mise a jour

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './templates/**/*.html.twig',
       './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'gn': '0 5px 10px 5px rgba(0,0,0,0.1)',
+      },
+      fontFamily: {
+        'roboto': ['roboto', defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
-    require('tw-elements/dist/plugin')
+    require('tw-elements/dist/plugin'),
+    require('tailwind-scrollbar')
   ],
+  variants: {
+    scrollbar: ['rounded']
+  }
 }
