67bdc1cdfa94302d09ddbdc9ed223a68306f8bd1
Pierre Denaes <photostudio13000@gmail.com>
2022-09-27 14:10:41 +0200

    modif story

--- src/Entity/Story.php
+++ src/Entity/Story.php
@@ -4,14 +4,24 @@ namespace App\Entity;
 
 use App\Repository\StoryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 #[ORM\Entity(repositoryClass: StoryRepository::class)]
+#[Vich\Uploadable]
 class Story
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
+    #[Vich\UploadableField(mapping: 'story', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(type: 'string')]
+    private ?string $imageName = null;
 
     #[ORM\Column(length: 255)]
     private ?string $title = null;
@@ -22,14 +32,46 @@ class Story
     #[ORM\Column]
     private ?bool $isActive = null;
 
-    #[ORM\ManyToOne(inversedBy: 'stories')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Profil $idProfil = null;
-
     public function getId(): ?int
     {
         return $this->id;
     }
+
+     /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
 
     public function getTitle(): ?string
     {
@@ -63,18 +105,6 @@ class Story
     public function setIsActive(bool $isActive): self
     {
         $this->isActive = $isActive;
-
-        return $this;
-    }
-
-    public function getIdProfil(): ?Profil
-    {
-        return $this->idProfil;
-    }
-
-    public function setIdProfil(?Profil $idProfil): self
-    {
-        $this->idProfil = $idProfil;
 
         return $this;
     }
