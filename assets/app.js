df523ed040d20fdd8930ad434792a63a9dda0e4c
VANDEN NESTE Bertrand <99867970+Bertrand-VANDEN-NESTE@users.noreply.github.com>
2022-09-28 13:45:58 +0200

    mise à jour

--- assets/app.js
+++ assets/app.js
@@ -14,6 +14,7 @@ import './bootstrap';
 // Import of tailwind elements
 import 'tw-elements';
 
+
 // carrousel album
 const items = [
     {
@@ -71,3 +72,31 @@ const options = {
         console.log('new slider item has been shown');
     }
 };
+
+const carousel = new Carousel(items, options);
+
+// goes to the next (right) slide
+carousel.next()
+
+// goes to the previous (left) slide
+carousel.prev()
+
+// jumps to the 3rd position (position starts from 0)
+carousel.slideTo(2)
+
+// starts or resumes the carousel cycling (automated sliding)
+carousel.cycle()
+
+// pauses the cycling (automated sliding)
+carousel.pause()
+
+const prevButton = document.getElementById('data-carousel-prev');
+const nextButton = document.getElementById('data-carousel-next');
+
+prevButton.addEventListener('click', () => {
+    carousel.prev();
+});
+
+nextButton.addEventListener('click', () => {
+    carousel.next();
+});
